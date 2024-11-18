6db14bd1b2932fbba911f7c55e29a94b8959e9e2
yusufoglu218 <yusufoglu218@gmail.com>
2024-11-18 21:22:57 +0300

    sql query to insert initial test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO customer (id, name, surname, email) VALUES (1, 'John', 'Doe', 'john@example.com');
+INSERT INTO asset (customer_id, asset_name, size, usable_size) VALUES (1, 'TRY', 10000, 10000);
+INSERT INTO asset (customer_id, asset_name, size, usable_size) VALUES (1, 'ASLL', 50, 50);
+
+INSERT INTO customer (id, name, surname, email) VALUES (2, 'Alice', 'Path', 'Alice@example.com');
+INSERT INTO asset (customer_id, asset_name, size, usable_size) VALUES (2, 'TRY', 5000, 5000);
+INSERT INTO asset (customer_id, asset_name, size, usable_size) VALUES (2, 'APPL', 150, 150);
