f00e8ea187ec4b8fb50f9ff15cc6927669ae0003
yusufoglu218 <yusufoglu218@gmail.com>
2024-11-18 21:13:45 +0300

    spring main application class

---
+++ src/main/java/com/brokerage/BrokerageApiApplication.java
@@ -0,0 +1,13 @@
+package com.brokerage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerageApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokerageApiApplication.class, args);
+    }
+
+}
